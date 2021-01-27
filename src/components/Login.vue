68dac8f1d8973324bdd1f38703e50098b912eb06
sx <240718221@qq.com>
2021-01-27 15:34:31 +0800

    样式简单布局

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-      login组件
+  <div class="login_container">
+      <div class="login_box"></div>
   </div>
 </template>
 
@@ -10,3 +10,21 @@ export default {
 }
 </script>
 
+<style lang="less" scoped>
+body{
+   .login_container{
+       background-color: #2b4b6b;
+       height: 100%;
+       .login_box{
+           width: 450px;
+           height: 300px;
+           background-color: #fff;
+           border-radius: 3px;
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50%,-50%);
+       }
+   }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router/index"
-import "./plugins/element.js"
 
+import "./plugins/element.js"
+import "./assets/css/global.css"
 Vue.config.productionTip = false
 
 new Vue({
