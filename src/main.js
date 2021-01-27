f190dd516bb6a47cbc595e2c6926497191ea17fd
sx <240718221@qq.com>
2021-01-27 18:51:47 +0800

    表单验证

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue"
 import router from "./router/index"
 
 import "./plugins/element.js"
+import input from "element-ui"
 import "./assets/css/global.css"
+//引入字体图标
+import "../fonts/iconfont.css"
 Vue.config.productionTip = false
-
+Vue.use(input)
 new Vue({
 	router,
 	render: h => h(App),
